a615fd51e8b6e96702e2a1a98722dcd19726b90a
timetake <1365173267w@gmail.com>
2021-06-06 16:24:04 -0400

    remove api bcrypt function, move to page component

--- pages/api/demo.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import bcrypt from 'bcrypt';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const password = '100';
-  const user_password = bcrypt.hashSync(password, 2);
-  console.log(user_password);
-};
