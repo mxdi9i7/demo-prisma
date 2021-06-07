b2c1f75dbb9c01abef20456c2784da597e2a925e
timetake <1365173267w@gmail.com>
2021-06-06 22:43:51 -0400

    remove prisma from frontend

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import { UserAddIcon } from '@heroicons/react/outline';
+import jwt from 'jsonwebtoken';
 
+const jwtSecret = process.env.JWT_SECRET;
 const prisma = new PrismaClient();
+const generateToken = (info: string) => jwt.sign(info, jwtSecret || '');
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { username, user_password, email } = JSON.parse(req.body);
-    const userData = {
-      username,
-      user_password: user_password,
-      email,
-    };
-    const savedUser = await prisma.temple_user.create({
-      data: userData,
-    });
-    res.json({ success: true, data: savedUser });
+    try {
+      const userDoc = await prisma.temple_user.findFirst({
+        where: {
+          OR: [{ username: username }, { email: email }],
+        },
+      });
+      if (userDoc) {
+        if (user_password == userDoc.user_password) {
+          throw 'Password Incorrect';
+        }
+      } else {
+        throw 'User Not Found';
+      }
+      console.log(generateToken(JSON.stringify(userDoc)));
+      const token = generateToken(JSON.stringify(userDoc));
+
+      res.json({ success: true, data: token });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false, data: error });
+    }
   } else {
     res.status(405).json({ message: 'Wrong method' });
   }
---
+++ pages/api/register.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { username, user_password, email } = JSON.parse(req.body);
+    const userData = {
+      username,
+      user_password: user_password,
+      email,
+    };
+    const savedUser = await prisma.temple_user.create({
+      data: userData,
+    });
+    res.json({ success: true, data: savedUser });
+  } else {
+    res.status(405).json({ message: 'Wrong method' });
+  }
+};
