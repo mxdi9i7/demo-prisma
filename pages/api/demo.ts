e15905378632c2128c26322de7bfa7e1d62a6659
timetake <1365173267w@gmail.com>
2021-06-06 22:06:59 -0400

    remove brypt add prisma

--- pages/api/demo.ts
+++ pages/api/demo.ts
@@ -4,8 +4,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const data = '111';
-    res.json({ success: true, data: data });
+    const { username, user_password, email } = JSON.parse(req.body);
+    const userData = {
+      username,
+      user_password: user_password,
+      email,
+    };
+    const savedUser = await prisma.temple_user.create({
+      data: userData,
+    });
+    res.json({ success: true, data: savedUser });
   } else {
     res.status(405).json({ message: 'Wrong method' });
   }
